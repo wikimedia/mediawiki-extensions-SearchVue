6b57fabf789428f2f7d766b51bff29ae646d6c5d
Simone Cuomo <simone@thisdot.co>
2022-10-06 13:31:52 +0100

    Add the ability for the Search preview to be closed on imageless pages
    - [x] Add Close button to image-less panels
    - [x] Remove Close button from Image component
    - [x] Remove unwanted mobile code (there is not current expectation of this to be re-stated)
    
    Bug: T319393
    Change-Id: I10f28a60b59a1501974b1b836c9d707aa452b4e0

--- resources/components/sections/QuickView.vue
+++ resources/components/sections/QuickView.vue
@@ -1,32 +1,34 @@
 <template>
 	<!-- eslint-disable -->
 	<div @click.stop>
-		<nav v-if="isMobile" class="mw-search-quick-view__mobile_nav">
+		<nav class="mw-search-quick-view__nav">
 			<button @click="closeQuickView">
-				<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+				<svg
+					width="30"
+					height="30"
+					viewBox="0 0 30 30"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
 					<title>
-						{{ $i18n( 'searchvue-close' ).text()  }}
+						{{ $i18n( 'searchvue-close' ).text() }}
 					</title>
-					<path d="M10 0a10 10 0 1 0 10 10A10 10 0 0 0 10 0zm5.66 14.24-1.41 1.41L10 11.41l-4.24 4.25-1.42-1.42L8.59 10 4.34 5.76l1.42-1.42L10 8.59l4.24-4.24 1.41 1.41L11.41 10z"/>
-				</svg>
-			</button>
-			<button @click="navigate( 'previous' )">
-				<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<title>
-						{{ $i18n( 'searchvue-previous' ).text()  }}
-					</title>
-					<path fill-rule="evenodd" clip-rule="evenodd" d="M9.75 0L0.75 9L9.75 18L11.25 16.5L3.75 9L11.25 1.5L9.75 0Z" fill="#202122"/>
-				</svg>
-			</button>
-			<button @click="navigate( 'next' )">
-				<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<title>
-						{{ $i18n( 'searchvue-next' ).text()  }}
-					</title>
-					<path fill-rule="evenodd" clip-rule="evenodd" d="M6.25 1L4.75 2.5L12.249 10L4.75 17.5L6.25 19L15.25 10L6.25 1Z" fill="#202122"/>
+					<circle
+						cx="15"
+						cy="15"
+						r="15"
+						fill="white"
+					/>
+					<path
+						fill-rule="evenodd"
+						clip-rule="evenodd"
+						d="M16.4122 14.998L22.7762 8.634L21.3622 7.22L14.9982 13.584L8.63621 7.222L7.22221 8.636L13.5842 14.998L7.22021 21.362L8.63421 22.776L14.9982 16.412L21.3642 22.778L22.7782 21.364L16.4122 14.998Z"
+						fill="#202122"
+					/>
 				</svg>
 			</button>
 		</nav>
+		
 		<header>
 			<quick-view-image
 				v-if="currentResult.thumbnail"
@@ -114,24 +116,31 @@ module.exports = exports = {
 
 <style lang="less">
 .mw-search-quick-view {
-	&__mobile_nav {
+	&__nav {
+		position: absolute;
+		left: 0;
+		right: 0;
+		width: 100%;
 		display: flex;
-		justify-content: flex-end;
-		height: 48px;
-		box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.1), inset 0px -1px 0px #C8CCD1;
+		justify-content: flex-start;
+		z-index: 10;
 
 		button {
 			padding: 14px;
-		}
+			padding-inline-start: 20px;
+			background-color: transparent;
+			border: none;
+			cursor: pointer;
 
-		button:first-child {
-			// This is required to display the first button on the far left
-			margin-right: auto;
+			svg {
+				fill: #fff;
+			}
 		}
 	}
 
 	header {
 		padding-bottom: 24px;
+		min-height: 32px;
 	}
 }
 </style>
--- resources/components/sections/QuickViewImage.vue
+++ resources/components/sections/QuickViewImage.vue
@@ -1,30 +1,5 @@
 <template>
 	<div class="QuickViewImage">
-		<button @click="$emit( 'close' )">
-			<svg
-				width="30"
-				height="30"
-				viewBox="0 0 30 30"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<title>
-					{{ $i18n( 'searchvue-close' ).text() }}
-				</title>
-				<circle
-					cx="15"
-					cy="15"
-					r="15"
-					fill="white"
-				/>
-				<path
-					fill-rule="evenodd"
-					clip-rule="evenodd"
-					d="M16.4122 14.998L22.7762 8.634L21.3622 7.22L14.9982 13.584L8.63621 7.222L7.22221 8.636L13.5842 14.998L7.22021 21.362L8.63421 22.776L14.9982 16.412L21.3642 22.778L22.7782 21.364L16.4122 14.998Z"
-					fill="#202122"
-				/>
-			</svg>
-		</button>
 		<image-with-loading-background
 			:src="source"
 			:alt="alt"
@@ -88,19 +63,6 @@ module.exports = exports = {
 	img {
 		width: 100%;
 		height: auto;
-	}
-
-	button {
-		position: absolute;
-		top: 12px;
-		left: 12px;
-		background: none;
-		border: none;
-		cursor: pointer;
-
-		svg {
-			fill: #fff;
-		}
 	}
 }
 </style>
